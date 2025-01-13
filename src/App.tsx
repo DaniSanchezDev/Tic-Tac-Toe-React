2dbaf2c7c72735389e94733e5745c73c78d26218
imdaano <danisanchezpsc@gmail.com>
2025-01-13 09:55:09 +0100

    FEAT add logic to start the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import NewGameButton from './components/NewGameButton'
 
+
+const PLAYER_1 = 1;
+const PLAYER_2 = 2;
+
+
 function App() {
+
+  const [turn, setTurn] = useState<number>();
+
+  const generateTurn = () => Math.floor(Math.random()* 2) + 1;
+
+  useEffect(() => {
+    setTurn(generateTurn());
+  }, [])
 
   return (
     <div className='game'>
       <Header />
       <NewGameButton />
+
     </div>
   )
 }
