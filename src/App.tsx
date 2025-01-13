89fa7f4279f47694491db293a0a6281f0a460e72
imdaano <danisanchezpsc@gmail.com>
2025-01-13 09:37:20 +0100

    FEAT add button to new game

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 
 import './App.css'
 import Header from './components/Header'
+import NewGameButton from './components/NewGameButton'
 
 function App() {
 
   return (
     <>
       <Header />
-      
+      <NewGameButton />
     </>
   )
 }
---
+++ src/components/NewGameButton/NewGameButton.tsx
@@ -0,0 +1,11 @@
+import './NewGameButton.css'
+
+function NewGameButton() {
+  return (
+    <div>
+        <button type='button'>New game</button>
+    </div>
+  )
+}
+
+export default NewGameButton
